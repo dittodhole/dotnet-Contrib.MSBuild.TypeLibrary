7b0dcf321b528a5631a75559c3db78d3117b1306
Andreas Niedermair <dev@andreas.niedermair.name>
2018-07-31 13:44:35 +0200

    added GetTlbExpPath

---
+++ src/Contrib.MSBuild.TypeLibrary/build/GetTlbExpPath.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+// ReSharper disable CheckNamespace
+
+namespace Contrib.MSBuild.TypeLibrary
+{
+  public partial class GetTlbExpPath : Task
+  {
+    public const string TlbExpFileName = "TlbExp.exe";
+
+    [Output]
+    public virtual string Path { get; set; }
+
+    /// <inheritdoc />
+    public override bool Execute()
+    {
+      this.Path = ToolLocationHelper.GetPathToDotNetFrameworkSdkFile(GetTlbExpPath.TlbExpFileName,
+                                                                     TargetDotNetFrameworkVersion.VersionLatest,
+                                                                     VisualStudioVersion.VersionLatest,
+                                                                     DotNetFrameworkArchitecture.Current);
+
+      var result = !string.IsNullOrEmpty(this.Path);
+
+      return result;
+    }
+  }
+}
