f956c2785f77ad2038e7dcc6b2ee57daa1be0d19
Andreas Niedermair <dev@andreas.niedermair.name>
2019-10-28 10:28:53 +0100

    restructured project

---
+++ src/Contrib.MSBuild.TypeLibrary/RegAsm.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using JetBrains.Annotations;
+
+namespace Contrib.MSBuild.TypeLibrary
+{
+  public sealed class RegAsm : ToolTask
+  {
+    [Required]
+    [NotNull]
+    public string Target { get; set; }
+
+    /// <inheritdoc/>
+    protected override string GenerateFullPathToTool()
+    {
+      return ToolLocationHelper.GetPathToDotNetFrameworkFile(this.ToolExe,
+                                                             TargetDotNetFrameworkVersion.VersionLatest,
+                                                             DotNetFrameworkArchitecture.Current);
+    }
+
+    /// <inheritdoc/>
+    protected override string ToolName
+    {
+      get
+      {
+        return "RegAsm.exe";
+      }
+    }
+
+    /// <inheritdoc/>
+    protected override string GenerateCommandLineCommands()
+    {
+      var commandLineBuilder = new CommandLineBuilder();
+
+      commandLineBuilder.AppendFileNameIfNotNull(this.Target);
+      commandLineBuilder.AppendSwitchIfNotNull("/regfile:", System.IO.Path.ChangeExtension(this.Target, ".reg"));
+
+      var result = commandLineBuilder.ToString();
+
+      return result;
+    }
+  }
+}
---
+++ src/Contrib.MSBuild.TypeLibrary/TlbExp.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using JetBrains.Annotations;
+
+namespace Contrib.MSBuild.TypeLibrary
+{
+  public sealed class TlbExp : ToolTask
+  {
+    [Required]
+    [NotNull]
+    public string Target { get; set; }
+
+    /// <inheritdoc/>
+    protected override string GenerateFullPathToTool()
+    {
+      return ToolLocationHelper.GetPathToDotNetFrameworkSdkFile(this.ToolExe,
+                                                                TargetDotNetFrameworkVersion.VersionLatest,
+                                                                DotNetFrameworkArchitecture.Current);
+    }
+
+    /// <inheritdoc/>
+    protected override string ToolName
+    {
+      get
+      {
+        return "TlbExp.exe";
+      }
+    }
+
+    /// <inheritdoc/>
+    protected override string GenerateCommandLineCommands()
+    {
+      var commandLineBuilder = new CommandLineBuilder();
+
+      commandLineBuilder.AppendFileNameIfNotNull(this.Target);
+      commandLineBuilder.AppendSwitchIfNotNull("/out:", System.IO.Path.ChangeExtension(this.Target, ".tlb"));
+
+      var result = commandLineBuilder.ToString();
+
+      return result;
+    }
+  }
+}
